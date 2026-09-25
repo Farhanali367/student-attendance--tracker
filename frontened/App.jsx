77bb051f804563bdfbf50d3db9d4e7b4b5903ab4
Mohd Farhan <mohdfarhan098ali@gmail.com>
2026-09-25 22:26:00 +0530

    Add login functionality to App component

--- frontened/App.jsx
+++ frontened/App.jsx
@@ -1,6 +1,19 @@
-import Attendance from "./Attendance";
+import { useState } from "react";
+import Login from "./Login.jsx";
+import Attendance from "./Attendance.jsx";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  if (!loggedIn) {
+    return (
+      <Login
+        onLogin={() => setLoggedIn(true)}
+        onSignup={() => alert("Signup page coming soon")}
+      />
+    );
+  }
+
   return <Attendance />;
 }
 
