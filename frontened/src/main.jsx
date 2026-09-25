f6c34b5ccd0f4f0a29758e3f987aabc2dfb3b3dc
Mohd Farhan <mohdfarhan098ali@gmail.com>
2026-09-25 22:33:38 +0530

    Refactor App component to manage page state

--- frontened/src/main.jsx
+++ frontened/src/main.jsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
+import Welcome from "./Welcome.jsx";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+import Dashboard from "./Dashboard.jsx";
+
+import "./index.css";
+
+function App() {
+  const [page, setPage] = useState("welcome");
+
+  if (page === "login") {
+    return (
+      <Login
+        onLogin={() => setPage("dashboard")}
+        onSignup={() => setPage("signup")}
+      />
+    );
+  }
+
+  if (page === "signup") {
+    return (
+      <Signup
+        onBackToLogin={() => setPage("login")}
+      />
+    );
+  }
+
+  if (page === "dashboard") {
+    return <Dashboard />;
+  }
+
+  return (
+    <Welcome
+      onLogin={() => setPage("login")}
+      onSignup={() => setPage("signup")}
+    />
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
